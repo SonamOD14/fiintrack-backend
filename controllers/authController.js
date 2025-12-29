ee4f3e872f875a015f4a435334c8decc5422326b
SonamOD14 <wangdisonam123456789@gmail.com>
2025-12-29 22:21:29 +0545

    verify email, forget password, and  reset password

---
+++ controllers/authController.js
@@ -0,0 +1,176 @@
+const User = require("../models/UserModels");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const sendEmail = require("../helper/sendEmail");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res) => {
+
+  try {
+    const { username, email, password, role } = req.body;
+
+    if (!username || !email || !password || !role) {
+      return res.status(400).json({
+        message: "all field must fill"
+      });
+    }
+
+    //search for samilar email
+    const user = await User.findOne({ where: { email: email } })
+    if (user) {
+      return res.status(400).json({
+        message: "user already exists"
+      })
+    }
+
+    //generation verification 
+    const verificationToken = crypto.randomBytes(32).toString("hex");
+
+    //verificationtoken expires time 
+    const verificationTokenExpires = new Date(Date.now() + 1 * 60 * 60 * 1000);
+
+    //fro hashing password
+    const hashedPassword = bcrypt.hashSync(password, 10)
+
+
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+      role,
+      verificationToken,
+      TokenExpires: verificationTokenExpires
+    })
+
+    const verifyLink = `http://localhost:3000/api/user/verify-email?token=${verificationToken}`
+
+    await sendEmail(
+      email,
+      "verify your email address",
+
+      `
+       <h2> verify ur self <h2>
+       <p>click to cerify the email</p>
+       <a href= ${verifyLink}>click here to verify </a>
+      `
+    )
+
+    return res.status(201).json({
+      message: `user register successfully`,
+      user: newUser
+    })
+
+
+  } catch (error) {
+    return res.json({
+      message: "server error",
+      error: error.message
+    })
+  }
+}
+
+const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "all filed are require"
+      });
+    }
+
+    const user = await User.findOne({ where: { email: email } });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "email not found"
+      });
+    }
+
+    const isMatch = bcrypt.compare(password, user.password)
+
+    if (isMatch) {
+
+      //jwt token generator
+      const token = jwt.sign(
+        {
+          id: user.id,
+          role: user.role
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: process.env.JWT_EXPIRES_IN
+        }
+      );
+
+      return res.status(201).json({
+        message: "you are logged in",
+        token: token
+      });
+
+    } else {
+      return res.status(400).json({
+        message: "email or password didn't match"
+      });
+    }
+
+  } catch (error) {
+    return res.status(500).json({
+      message: "server side error",
+      error: error.message
+    })
+  }
+}
+
+const forgetPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    //email khali chodnu banaya na 
+    if (!email) {
+      return res.status(400).json({
+        message: "email is required"
+      })
+    }
+
+    const user = await User.findOne({ where: { email: email } });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "user with this email does not exist"
+      })
+    }
+
+    const resetToken = crypto.randomBytes(32).toString("hex");
+
+    //Date.now() → current time  kena ke token should not live forever 
+    const resetTokenExpires = new Date(Date.now() + 1 * 60 * 60 * 1000);
+
+    user.verificationToken = resetToken;
+    user.TokenExpires = resetTokenExpires;
+    await user.save();
+
+    const resetLink = `http://localhost:3000/api/user/reset-password?token=${resetToken}`;
+
+    await sendEmail(
+      email,
+      "Reset your password",
+      `
+        <h2>Password Reset</h2>
+        <p>Click the link below to reset your password:</p>
+        <a href="${resetLink}">Reset Password</a>
+      `
+    );
+    return res.status(200).json({
+      message: "password reset link send to your email"
+    })
+
+  } catch (error) {
+    return res.status(500).json({
+      message: "server error",
+      error: error.message
+    });
+  }
+}
+
+module.exports = { registerUser, userLogin, forgetPassword };
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
+const verifyEmail = require("../controllers/verifyEmail");
+const resetPassword = require("../helper/resetPassword")
 
-const registerUser = require("../controllers/userController");
+const { registerUser, userLogin, forgetPassword } = require("../controllers/authController");
 
 router.post("/register", registerUser);
+router.get("/verify-email", verifyEmail);
+router.post("/login", userLogin);
+router.post("/forgetPassword", forgetPassword);
+router.post("/reset-password", resetPassword);
+
 
 
 module.exports = router;
