7a034cc5432deced4d218dfdc393e4557394e145
SonamOD14 <wangdisonam123456789@gmail.com>
2026-01-19 20:43:08 +0545

    models/ExpenseModel.js

--- models/ExpenseModel.js
+++ models/ExpenseModel.js
@@ -1,44 +1,57 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../db/Database");
 
-const Expense = sequelize.define(
-  "ExpenseModel",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-
-    amount: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false
-    },
-
-    category: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-
-    expenseDate: {
-      type: DataTypes.DATEONLY,
-      allowNull: false
-    },
-
-    note: {
-      type: DataTypes.STRING,
-      allowNull: true
-    }
+const Expense = sequelize.define("ExpenseModel", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
   },
-  {
-    tableName: "expenses",
-    timestamps: true
+
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+  amount: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+
+  type: {
+    type: DataTypes.ENUM("expense", "income"),
+    defaultValue: "expense",
+    allowNull: false
+  },
+
+  category: {
+    type: DataTypes.ENUM(
+      "Food",
+      "Transport",
+      "Shopping",
+      "Entertainment",
+      "Healthcare",
+      "Bills"
+    ),
+    allowNull: false
+  },
+
+  expenseDate: {
+    type: DataTypes.DATEONLY,
+    allowNull: false
+  },
+
+  note: {
+    type: DataTypes.STRING
+  },
+
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
   }
-);
+}, {
+  tableName: "expenses",
+  timestamps: true
+});
 
 module.exports = Expense;
